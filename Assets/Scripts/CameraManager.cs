a24f8771a1812663865a765c2e5836fd98b49820
Michael Holwey <mholwey3@hotmail.com>
2018-02-11 14:26:15 -0600

    Added player movement and rotation and collision detection.

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public Vector3 offset;
+	public float lerpSpeed;
+
+	private GameObject player;
+
+	void Start() {
+		player = GameObject.FindGameObjectWithTag("Player");
+		transform.position = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+	}
+	
+	void FixedUpdate () {
+		Vector3 start = transform.position;
+		Vector3 end = new Vector3(player.transform.position.x + offset.x, player.transform.position.y + offset.y, player.transform.position.z + offset.z);
+		transform.position = Vector3.Lerp(start, end, lerpSpeed);
+		transform.LookAt(player.transform);
+	}
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	public float maxVelocity;
+	public float thrust;
+	public float deceleration;
+	public float rotationSpeed;
+
+	private Rigidbody rigidbody;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	void FixedUpdate () {
+		// MOVEMENT
+		if (Input.GetButton("Vertical")){
+			rigidbody.AddForce(Input.GetAxis("Vertical") * transform.up * thrust);
+			if (rigidbody.velocity.magnitude > maxVelocity) {
+				rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVelocity);
+			}
+		}
+
+		//ROTATION
+		if (Input.GetButton("Horizontal")) {
+			transform.Rotate(Input.GetAxis("Horizontal") * Vector3.back * rotationSpeed);
+		}
+	}
+}
