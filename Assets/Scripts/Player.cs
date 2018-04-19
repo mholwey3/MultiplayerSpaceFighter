06b72c745075502d2cc92da57093184c1265b7cd
Michael Holwey <mholwey3@hotmail.com>
2018-04-18 19:09:35 -0500

    Pulled in some space ship prefabs with which to test. Started creating map/mesh generator for procedurally generated environments.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,17 +9,30 @@ public class Player : MonoBehaviour {
 	public float rotationSpeed;
 
 	private Rigidbody rigidbody;
+	private ParticleSystem leftExhaust;
+	private ParticleSystem rightExhaust;
 
 	void Start () {
 		rigidbody = GetComponent<Rigidbody>();
+		leftExhaust = transform.GetChild(0).GetComponent<ParticleSystem>();
+		rightExhaust = transform.GetChild(1).GetComponent<ParticleSystem>();
 	}
 	
 	void FixedUpdate () {
 		// MOVEMENT
 		if (Input.GetButton("Vertical")){
+			if (!leftExhaust.isPlaying && !rightExhaust.isPlaying && Input.GetAxis("Vertical") > 0f) {
+				leftExhaust.Play();
+				rightExhaust.Play();
+			}
 			rigidbody.AddForce(Input.GetAxis("Vertical") * transform.up * thrust);
 			if (rigidbody.velocity.magnitude > maxVelocity) {
 				rigidbody.velocity = Vector3.ClampMagnitude(rigidbody.velocity, maxVelocity);
+			}
+		} else {
+			if (leftExhaust.isPlaying && rightExhaust.isPlaying) {
+				leftExhaust.Stop();
+				rightExhaust.Stop();
 			}
 		}
 
